2197242773868eb58dea980104a8745e34c9c05f
Steven Rogalsky <srogalsky8@gmail.com>
2019-05-09 17:29:25 -0700

    Updates

---
+++ precache-manifest.a0990b7f5528ea1fb90814ea7253a67d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff321eeb79e6e4a3b08c9b766719fe5",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "2a956fcb8adf9566e7fd",
+    "url": "/pokedex/static/css/main.69ae9577.chunk.css"
+  },
+  {
+    "revision": "40dcd6e620393a79aa47",
+    "url": "/pokedex/static/js/2.5a10c014.chunk.js"
+  },
+  {
+    "revision": "2a956fcb8adf9566e7fd",
+    "url": "/pokedex/static/js/main.07d8c5db.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
